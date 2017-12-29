8b811856d472dcbd296ff8cec2314dc73d4a2a29
liuzj_jj@163.com <liuzj_jj@163.com>
2017-12-29 12:21:26 +0800

    1235678

--- common/src/main/java/com/yumu/hexie/integration/wuye/WuyeUtil.java
+++ common/src/main/java/com/yumu/hexie/integration/wuye/WuyeUtil.java
@@ -190,13 +190,13 @@ public class WuyeUtil {
 	//16.商品支付
 	public static BaseResult<JsSign> getOrderPay(String trade_no, String openId, String return_url, String price) throws ValidationException {
 		String url = REQUEST_ADDRESS + String.format(ORDER_PAY_URL, trade_no, CSPID, openId, return_url, price);
-		BaseResult<String> result = (BaseResult<String>)httpGet(url, String.class);
+		BaseResult<JsSign> result = (BaseResult<JsSign>)httpGet(url, JsSign.class);
 		
+		Log.debug(">>>>>>>>>>>>>>result: "+ result);
 		if (!result.isSuccess()) {
 			throw new ValidationException(result.getData().toString());
 		}
-		return (BaseResult<JsSign>)httpGet(url, JsSign.class);
-		
+		return result;
 	}
 	
 	//17.商品支付通知查询
@@ -209,9 +209,8 @@ public class WuyeUtil {
 		
 		if (!result.isSuccess() && !StringUtil.isEmpty(result.getData())) {
 			throw new ValidationException(result.getData().toString());
-		}else {
-			return (BaseResult<JSONObject>)httpGet(url, String.class);
 		}
+		return result;
 	}
 	
 	//18.商品支付退款
