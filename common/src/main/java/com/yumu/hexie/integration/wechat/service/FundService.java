8dc687b6cd366dbe4a7abece5f63b3754578f67f
liuzj_jj@163.com <liuzj_jj@163.com>
2018-07-11 17:51:47 +0800

    购物车

--- common/src/main/java/com/yumu/hexie/integration/wechat/service/FundService.java
+++ common/src/main/java/com/yumu/hexie/integration/wechat/service/FundService.java
@@ -5,8 +5,6 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
 
-import javax.xml.bind.ValidationException;
-
 import org.json.JSONObject;
 
 import com.yumu.hexie.common.util.JacksonJsonUtil;
@@ -33,49 +31,9 @@ public class FundService {
 	 * 商品描述:body
 	 * @param db
 	 * @return
-	 * @throws ValidationException 
 	 */
-	public static JsSign createOrder(PaymentOrder payOrder, String return_url) throws ValidationException{
-		
+	public static JsSign createOrder(PaymentOrder payOrder, String return_url){
 		return WuyeUtil.getOrderPay(payOrder.getPaymentNo()+"", payOrder.getOpenId(), return_url, payOrder.getPrice()+"").getData();
-		
-//		InetAddress addr = null;
-//		try {
-//			addr = InetAddress.getLocalHost();
-//		} catch (UnknownHostException e) {
-//			e.printStackTrace();
-//			//throw new AppSystemException(e);
-//		} 
-//		
-////		notify_url = "http://180.168.39.14/";	//TODO 暂时用财付通的测试
-//		
-//		String spbill_create_ip = addr.getHostAddress().toString();	//订单生成的机器IP
-//		String nonce_str = WeixinUtil.buildRandom();	//随机字符串
-//		Map<String,String> map = new HashMap<String, String>();
-//		map.put("appid", ConstantWeChat.APPID_PAY);
-//		map.put("mch_id", ConstantWeChat.MERCHANTID);
-//		map.put("notify_url", ConstantWeChat.NOTIFYURL);
-//		map.put("trade_type", ConstantWeChat.TRADETYPE);
-//		
-//		map.put("spbill_create_ip", spbill_create_ip);
-//		map.put("nonce_str", nonce_str);
-//		
-//		map.put("out_trade_no", payOrder.getPaymentNo());
-//
-//		DecimalFormat decimalFormat=new DecimalFormat("0");
-//		String price = decimalFormat.format(payOrder.getPrice()*100);
-//		map.put("total_fee", price);
-//		map.put("body", payOrder.getProductName());
-//		map.put("openid", payOrder.getOpenId());
-//		
-//		String sign = WeixinUtil.createSign(map, ConstantWeChat.KEY);	//生成签名
-//		map.put("sign", sign);
-//		map.remove("key");	//只有生成签名的时候需要将key加入，组建XML时无须使用KEY，不然会报错。
-//		String requestXml = MessageUtil.createPayRequestXML(map);
-//		//String requestXml = JacksonJsonUtil.mapToXml(map);
-//		PrePaymentOrder r = (PrePaymentOrder)WeixinUtil.httpsRequestXml(
-//				UNIPAY_URL,  "POST", requestXml,PrePaymentOrder.class);
-//		return r; 
 	}
 	
 	/**
@@ -89,10 +47,9 @@ public class FundService {
 	 * 	err_code	错误代码（对应业务结果）
 	 * 	err_code_des错误描述（对应业务结果）
 	 * 	trade_state 交易状态
-	 * @throws ValidationException 
 	 * 
 	 */
-	public static JSONObject queryOrder(String out_trade_no) throws ValidationException{
+	public static JSONObject queryOrder(String out_trade_no){
 		return WuyeUtil.notifyPayed(out_trade_no).getData();
 	}
 	
@@ -162,15 +119,15 @@ public class FundService {
 	public static JsSign getPrepareSign(String prepay_id) {
 		Map<String,String> map = new HashMap<String, String>();
 		JsSign r = new JsSign();
-		r.setAppId(ConstantWeChat.APPID);
+		r.setAppId(ConstantWeChat.APPID_PAY);
 		r.setTimestamp(""+(int)(System.currentTimeMillis()/1000));
 		r.setNonceStr(WeixinUtil.buildRandom());
-		r.setPkgStr(prepay_id);
+		r.setPkgStr("prepay_id=" + prepay_id);
 		
 		map.put("appId", r.getAppId());
 		map.put("timeStamp", r.getTimestamp());
 		map.put("nonceStr", r.getNonceStr());
-		map.put("package", prepay_id);
+		map.put("package", "prepay_id=" + prepay_id);
 		map.put("signType", "MD5");
 		r.setSignature(WeixinUtil.createSign(map, ConstantWeChat.KEY));
 		return r;
