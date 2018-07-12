775c747ea49e17a6e4211390290a4d1283c79b3a
wangyw <532858859@qq.com>
2018-07-12 12:05:29 +0800

    更新解决冲突

--- common/src/main/java/com/yumu/hexie/integration/wuye/WuyeUtil.java
+++ common/src/main/java/com/yumu/hexie/integration/wuye/WuyeUtil.java
@@ -4,19 +4,17 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Map;
 import java.util.Properties;
-
 import javax.xml.bind.ValidationException;
-
 import org.apache.http.client.methods.HttpGet;
 import org.hibernate.bytecode.buildtime.spi.ExecutionException;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import com.fasterxml.jackson.databind.JavaType;
 import com.yumu.hexie.common.util.JacksonJsonUtil;
 import com.yumu.hexie.common.util.MyHttpClient;
+import com.yumu.hexie.integration.wechat.entity.common.JsSign;
 import com.yumu.hexie.integration.wuye.resp.BaseResult;
 import com.yumu.hexie.integration.wuye.resp.BillListVO;
 import com.yumu.hexie.integration.wuye.resp.CellListVO;
@@ -186,9 +184,9 @@ public class WuyeUtil {
 	}
 	
 	//16.商品支付
-	public static BaseResult<String> getOrderPay(String trade_no, String openId, String return_url, String price) {
+	public static BaseResult<JsSign> getOrderPay(String trade_no, String openId, String return_url, String price) {
 		String url = REQUEST_ADDRESS + String.format(ORDER_PAY_URL, trade_no, CSPID, openId, return_url, price);
-		return (BaseResult<String>)httpGet(url, String.class);
+		return (BaseResult<JsSign>)httpGet(url, JsSign.class);
 	}
 	
 	//17.商品支付通知查询
