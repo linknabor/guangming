bc0fb29634c53da1e45a77cc6112c29dffddadd0
wangyw <532858859@qq.com>
2018-08-06 15:47:17 +0800

    京东

--- common/src/main/java/com/yumu/hexie/service/jingdong/impl/JDProductServiceImpl.java
+++ common/src/main/java/com/yumu/hexie/service/jingdong/impl/JDProductServiceImpl.java
@@ -757,6 +757,8 @@ public class JDProductServiceImpl implements JDProductService{
 												
 												if(regionj.get(i).getRegion_name()=="上海") {
 													String regionname=regionj.get(i).getInfo().get(j2).getRegion_name();
+													logger.info(regionname+"-----------"+region.get(j).getInfo().get(k).getInfo().get(l).getName());
+													logger.info(regionj.get(i).getRegion_name());
 													if(regionname==region.get(j).getInfo().get(k).getInfo().get(l).getName()||regionname.equals(region.get(j).getInfo().get(k).getInfo().get(l).getName())) {
 														JDregionMapping jdregionmapping = new JDregionMapping();
 														jdregionmapping.setJdparentid(Integer.parseInt(regionj.get(i).getInfo().get(j2).getParent_id()));
