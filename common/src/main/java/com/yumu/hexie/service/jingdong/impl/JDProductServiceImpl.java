2ae8cb087cfcdd263779d0b4c5d0beea469ea972
wangyw <532858859@qq.com>
2018-08-06 16:08:46 +0800

    京东

--- common/src/main/java/com/yumu/hexie/service/jingdong/impl/JDProductServiceImpl.java
+++ common/src/main/java/com/yumu/hexie/service/jingdong/impl/JDProductServiceImpl.java
@@ -731,15 +731,14 @@ public class JDProductServiceImpl implements JDProductService{
 				//判断市是否一样
 				if(regionj.get(i).getRegion_name().equals(region.get(j).getName())||regionj.get(i).getRegion_name()==region.get(j).getName()) {
 					
-					//拿到市里面的区
-					for (int j2 = 0; j2 < regionj.get(i).getInfo().size(); j2++) {
-						for (int k = 0; k < region.get(j).getInfo().size(); k++) {
+					if(regionj.get(i).getRegion_name()=="上海") {
+						for (int j2 = 0; j2 < regionj.get(i).getInfo().size(); j2++) {
 							
 							
-							if(regionj.get(i).getRegion_name()=="上海") {
+							for (int k = 0; k < region.get(j).getInfo().size(); k++) {
 								for (int l = 0; l < region.get(j).getInfo().get(k).getInfo().size(); l++) {
 									String regionname=regionj.get(i).getInfo().get(j2).getRegion_name();
-									logger.info(regionname+"-----------"+region.get(j).getInfo().get(k).getInfo().get(l).getName());
+									logger.info(regionname+"-----------");
 									logger.info(regionj.get(i).getRegion_name());
 									if(regionname==region.get(j).getInfo().get(k).getInfo().get(l).getName()||regionname.equals(region.get(j).getInfo().get(k).getInfo().get(l).getName())) {
 										JDregionMapping jdregionmapping = new JDregionMapping();
@@ -753,6 +752,12 @@ public class JDProductServiceImpl implements JDProductService{
 									}
 								}
 							}
+						}
+					}
+					//拿到市里面的区
+					for (int j2 = 0; j2 < regionj.get(i).getInfo().size(); j2++) {
+						for (int k = 0; k < region.get(j).getInfo().size(); k++) {
+							
 							
 							//判断区是否一样
 							if(regionj.get(i).getInfo().get(j2).getRegion_name().equals(region.get(j).getInfo().get(k).getName())||regionj.get(i).getInfo().get(j2).getRegion_name()==region.get(j).getInfo().get(k).getName()) {
