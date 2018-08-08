3f200c62098a5b362889bc4103788bcdc87319e5
wangyw <532858859@qq.com>
2018-08-08 14:38:24 +0800

    京东

--- hexie/src/main/java/com/yumu/hexie/web/product/JDPushController.java
+++ hexie/src/main/java/com/yumu/hexie/web/product/JDPushController.java
@@ -2,6 +2,7 @@ package com.yumu.hexie.web.product;
 
 import javax.inject.Inject;
 
+import org.json.JSONException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.yumu.hexie.common.util.JacksonJsonUtil;
 import com.yumu.hexie.model.jingdong.JDReceiveVO;
 import com.yumu.hexie.service.jingdong.JDProductService;
 @Controller
@@ -22,12 +24,21 @@ public class JDPushController {
 	
 	@RequestMapping(value = "/Verification", method = RequestMethod.POST)
 	@ResponseBody
-	public String Verification(@RequestBody JDReceiveVO jdreceive) {
-		if(jdproductService.verificationJD(jdreceive)) {
+	public String Verification(String param) {
+		
+		JDReceiveVO jdr = null;
+		try {
+			jdr = (JDReceiveVO)JacksonJsonUtil.jsonToBean(param, JDReceiveVO.class);
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			log.error(e.toString());
+			e.printStackTrace();
+		}
+		if(jdproductService.verificationJD(jdr)) {
 			log.info("---订单验证成功---");
 			return "success";
 		}else {
-			log.error("订单验证失败-----第三方订单号："+jdreceive.getThridsn()+"  网壕订单号："+jdreceive.getOrdersn()+"  协议价价格："+jdreceive.getOrder_amount());
+			log.error("订单验证失败-----第三方订单号："+jdr.getThridsn()+"  网壕订单号："+jdr.getOrdersn()+"  协议价价格："+jdr.getOrder_amount());
 			return "fail";
 		}
 	}
